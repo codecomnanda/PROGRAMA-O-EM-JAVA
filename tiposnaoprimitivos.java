e1ac3fb49a10aa5d73ddae7bc7ceec6a6583b1a0
FERNANDA GUIMARÃES <fernandainbox@gmail.com>
2026-05-04 16:04:26 -0300

    Update tiposnaoprimitivos.java

--- tiposnaoprimitivos.java
+++ tiposnaoprimitivos.java
@@ -2,6 +2,7 @@ public class tiposnaoprimitivos {
     public static void main(String[] args) {
         String mensagem = "Olá, Fernanda!";
         int[] numeros = {1, 2, 3, 4, 5};
+        double pi = 3.141597;
 
         System.out.println("Mensagem: " + mensagem);
         System.out.println("Primeiro número: " + numeros[0]);
