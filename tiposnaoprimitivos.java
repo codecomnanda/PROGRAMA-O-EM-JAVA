792a1da5600222b38bbb90d5f31fbdef1f56eb18
FERNANDA GUIMARÃES <fernandainbox@gmail.com>
2026-05-04 16:01:52 -0300

    Tipos Não Primitivos

---
+++ tiposnaoprimitivos.java
@@ -0,0 +1,9 @@
+public class tiposnaoprimitivos {
+    public static void main(String[] args) {
+        String mensagem = "Olá, Fernanda!";
+        int[] numeros = {1, 2, 3, 4, 5};
+
+        System.out.println("Mensagem: " + mensagem);
+        System.out.println("Primeiro número: " + numeros[0]);
+    }
+}
