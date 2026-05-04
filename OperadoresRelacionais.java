688a2e458595a4642db9ccd8ebd66824e6861e90
FERNANDA GUIMARÃES <fernandainbox@gmail.com>
2026-05-04 16:38:30 -0300

    Operadores relacionais

---
+++ OperadoresRelacionais.java
@@ -0,0 +1,16 @@
+public class OperadoresRelacionais {
+    public static void main(String[] args) {
+        int fernandaIdade = 41;
+        int teste = 18;
+
+
+        if (fernandaIdade >= teste) {
+            System.out.println("Maior de Idade.");
+        
+        } else {
+            System.out.println("Menor de Idade");
+        }
+    }
+    
+}
+
