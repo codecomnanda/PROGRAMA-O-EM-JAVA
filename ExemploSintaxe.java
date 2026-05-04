e9250893e27bfd88601ded39d5a5467ba09f517c
FERNANDA GUIMARÃES <fernandainbox@gmail.com>
2026-05-04 15:54:17 -0300

    commit

--- ExemploSintaxe.java
+++ ExemploSintaxe.java
@@ -1,6 +1,7 @@
 public class ExemploSintaxe {
     public static void main(String[] args) {
         int idade = 25;
+        double altura = 1.75;
         System.out.println("Idade: +idade");
     }
 }
---
+++ tiposprimitivos.java
@@ -0,0 +1,13 @@
+public class tiposprimitivos {
+    public static void main(String[] args) {
+        int numero = 10;
+        double preco = 5.99;
+        char letra = 'C';
+        boolean status = true;
+
+        System.out.println("Número: " + numero);
+        System.out.println("Preço: " + preco);
+        System.out.println("Letra: " + letra);
+        System.out.println("Status: " + status);
+    }
+}
