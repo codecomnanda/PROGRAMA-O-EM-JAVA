15adbdbc28f9d24a4e57f01eeb70783f67802836
FERNANDA GUIMARÃES <fernandainbox@gmail.com>
2026-05-13 17:24:01 -0300

    for

---
+++ ExemploFor.java
@@ -0,0 +1,10 @@
+public class ExemploFor {
+    public static void main(String[] args) {
+        // Uso do for: repetir um número fixo de vezes
+        System.out.println("Contagem regressiva:");
+        for (int i = 5; i >= 1; i--) {
+            System.out.println(i);
+        }
+        System.out.println("Lançamento!");
+    }
+}
