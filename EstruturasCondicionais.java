d5e1a768cab45bdfafb920dfb15a07e358b0e297
FERNANDA GUIMARÃES <fernandainbox@gmail.com>
2026-05-13 17:08:57 -0300

    Update EstruturasCondicionais.java

--- EstruturasCondicionais.java
+++ EstruturasCondicionais.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class VerificaNumero {
+public class EstruturasCondicionais {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Digite um número: ");
